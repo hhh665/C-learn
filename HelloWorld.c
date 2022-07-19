a37528a65a44e08841d202cd927832bc650b6b7f
h665 <5954747821@qq.com>
2022-07-19 16:48:24 +0800

    新增代码

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("Hello World!");
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("%d", sum);
+	return 0;
+}
