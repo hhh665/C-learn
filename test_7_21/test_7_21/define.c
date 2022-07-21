20c07acfc483bebf8b22a8f992aa4be6bab6c2d2
h665 <5954747821@qq.com>
2022-07-21 17:27:56 +0800

    提交代码

---
+++ test_7_21/test_7_21/define.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//
+//#define M 1000
+//
+//#define ADD(x, y) ((x)+(y))
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = ADD(a, b);
+//	printf("%d", M);
+//	printf("%d", sum);
+//
+//	return 0;
+
+//}
+//int main()
+//{
+//	int a = 10;
+//	printf("%p", &a);
+//	return 0;
+//}
+struct Stu
+{
+	char name[20];
+	int age;
+	char sex[5];
+	char id[12];
+};
+int main()
+{
+	struct Stu s = { "zhangsan", 20, "nan", "20211001155" };
+	printf("%s %d %s %s", s.name, s.age, s.sex, s.id);
+	return 0;
+}
