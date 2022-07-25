8336d293235636fa224419b84c125a984fdd81b9
h665 <594747821@qq.com>
2022-07-25 09:52:18 +0800

    补提交代码

---
+++ test_7_24/test_7_24/test_7_24.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//void mul(int num)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int ret = 0;
+//	for (i = 1; i <= num; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret = i * j;
+//			printf("%d*%d=%-2d ", i, j, ret);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	mul(a);
+//	return 0;
+//}
+
+int ass(int arr[])
+{
+	printf("%d", arr[5]);
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d", arr[5]);
+	ass(arr);
+	return 0;
+}
