5e813ef72fb2df68ca845cd6afbe87ba98c16c0a
h665 <594747821@qq.com>
2022-09-24 22:22:38 +0800

    提交代码

---
+++ test_9_24/test_9_24/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	char* p = "abcdef";
+//	
+//	return 0;
+//}
+
+int main()
+{
+	int a[4] = { 1,2,3,4 };
+	int* ptr1 = (int*)(&a + 1);
+	int* ptr2 = (int*)((int)a + 1);
+	printf("%x,%x", ptr1[-1], *ptr2);
+	return 0;
+}
